b9e5d2af08791112c0a094e9ca9ed48697ada225
Sinantrarion <vikbar199@gmail.com>
2023-01-25 22:57:18 +0200

    Character sheet now shows

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -107,18 +107,5 @@ export class FoundryMnM3eActor extends Actor {
    */
    _getCharacterRollData(data) {
     if (this.type !== 'hero') return;
-
-    // Copy the ability scores to the top level, so that rolls can use
-    // formulas like `@str.mod + 4`.
-    if (data.abilities) {
-      for (let [k, v] of Object.entries(data.abilities)) {
-        data[k] = foundry.utils.deepClone(v);
-      }
     }
-
-    // Add level for easier access, or fall back to 0.
-    if (data.attributes.level) {
-      data.lvl = data.attributes.level.value ?? 0;
-    }
-  }
 }
